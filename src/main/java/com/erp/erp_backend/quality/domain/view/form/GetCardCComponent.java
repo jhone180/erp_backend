6b437c17af33e48085212ecd50fc5b5f1ee08cae
Jhon Garcia <jhongg6@hotmail.com>
2024-04-02 18:42:16 -0500

    (feat): Se agrega nuevas clases para obtener la card

---
+++ src/main/java/com/erp/erp_backend/quality/domain/view/form/GetCardCComponent.java
@@ -0,0 +1,8 @@
+package com.erp.erp_backend.quality.domain.view.form;
+
+import com.erp.erp_backend.quality.domain.view.component.CardCComponent;
+
+import java.util.function.Supplier;
+
+public interface GetCardCComponent extends Supplier<CardCComponent> {
+}
---
+++ src/main/java/com/erp/erp_backend/quality/domain/view/form/impl/GetCardCComponentImpl.java
@@ -0,0 +1,11 @@
+package com.erp.erp_backend.quality.domain.view.form.impl;
+
+import com.erp.erp_backend.quality.domain.view.component.CardCComponent;
+import com.erp.erp_backend.quality.domain.view.form.GetCardCComponent;
+
+public class GetCardCComponentImpl implements GetCardCComponent {
+    @Override
+    public CardCComponent get() {
+        return null;
+    }
+}
